32b6a26ff2933cad045db7b13398bdcbea471f9d
restureese <restureese@gmail.com>
2018-05-03 08:25:18 +0700

    update

---
+++ KNN.py
@@ -0,0 +1,47 @@
+
+# coding: utf-8
+
+# # Load Dataset
+
+# In[ ]:
+
+import pandas as pd
+
+df = pd.read_csv('data.csv',header=None)
+print(df)
+
+
+# # Preprocessing
+
+# In[ ]:
+
+X = []
+y = []
+for (value,label) in zip(df[1],df[2]):
+    X.append([value])
+    y.append([label])
+
+
+# # Create Model
+
+# In[ ]:
+
+from sklearn.neighbors import KNeighborsClassifier
+
+n_neighbors = 3
+
+clf = KNeighborsClassifier(n_neighbors)
+
+clf.fit(X,y)
+
+
+# # Testing Model
+
+# In[ ]:
+
+datatest = pd.read_csv('testing.csv',header=None)
+
+for (laptop,harga) in zip(datatest[0],datatest[1]):
+    predict = clf.predict([[harga]])
+    print("laptop {laptop} dengan harga {harga} itu {hasil}".format(laptop=laptop,harga=harga,hasil=predict[0]))
+
